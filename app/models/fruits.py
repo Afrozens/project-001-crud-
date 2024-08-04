2c90944ead9c95133725607d971c7f09c652d5ae
Luis321123 <luis1233210e@gmail.com>
2024-08-04 17:37:32 -0500

    add model of fruit

--- app/models/fruits.py
+++ app/models/fruits.py
@@ -1,3 +1,22 @@
+from  app.core.database import Base
+from uuid import uuid4 
+from sqlalchemy import Boolean, Column, DateTime, String, func, Text, Integer
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 
+class Fruit(Base):
+    __tablename__= 'fruits'
 
+    id= Column( 
+        UUID(150), primary_key=True, index= True, default=uuid4
+    )
+    
+    name = Column(String(150))
+    description = Column(Text)
+    price = Column(Integer)
+    Featured = Column(Boolean, default=False)
+    finish_line_at = Column(DateTime)
+    place = Column(String(150),nullable=True)
+    
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
